b3a602b5c7144761e70fae0b7184aa0309a5addc
Juan Garrido Troche <j.garridotroche@gmail.com>
2023-03-20 00:21:43 +0100

    Creando los componentes de Header, Footer, Main y Home con sus correspondientes css, Mobile First y responsive

--- xpaccount/frontend/src/pages/Header.jsx
+++ xpaccount/frontend/src/pages/Header.jsx
@@ -1,10 +1,15 @@
-import ("../css/Header.css")
+import { useState } from "react";
+
+import("../css/Header.css");
 export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <header>
       <section className="brand-container">
         <img src="/wallet.svg" alt="xp account logo" />
-        <h3><span>xp</span>account</h3>
+        <h3>
+          <span>xp</span>account
+        </h3>
       </section>
       <section className="menu-container">
         <ul>
@@ -13,11 +18,13 @@ export const Header = () => {
           <li>Sobre nosotros</li>
         </ul>
       </section>
-      <section className="hamburger-container nav-toggle">
+      <section className={`nav-toggle ${isOpen && "open"}`} onClick={() => {
+        setIsOpen(!isOpen);        
+      }}>
         <span></span>
         <span></span>
         <span></span>
       </section>
     </header>
-  )
-}
+  );
+};
