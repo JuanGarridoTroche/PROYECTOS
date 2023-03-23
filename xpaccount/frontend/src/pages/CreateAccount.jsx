d677cac8809b57dab44a08cb394b2af6105eaa42
Juan Garrido Troche <j.garridotroche@gmail.com>
2023-03-23 23:55:50 +0100

    CREATE: createAccount, creación del formulario, validaciones en HTML y CSS

--- xpaccount/frontend/src/pages/CreateAccount.jsx
+++ xpaccount/frontend/src/pages/CreateAccount.jsx
@@ -1,24 +1,46 @@
+import { useState } from "react";
 
 export const CreateAccount = ()=> {
+  const [error, setError] = useState("")
+
+  const handleSubmit = async (e)=> {
+    e.preventDefault();
+    setError("");
+    try {
+      
+    } catch (error) {
+      setError(error.message);
+    }
+  }
 
   return(
     <section className="create-account-container">
       <h2>Crear una cuenta nueva</h2>
-      <form>
-        <label htmlFor="alias">Escribe un nombre (alias) para tu nueva cuenta</label>
-        <input type="text" name="alias" id="alias" required/>
-        <label htmlFor="bankName">Banco</label>
-        <input type="text" name="bankName" id="bankName"/>
-        <label htmlFor="ibanCode"></label>
-        <input type="text" name="ibanCode" id="ibanCode" required pattern="[A-Z]{2}[0-9]{2}"/>
-        <label htmlFor="entityCode"></label>
-        <input type="text" name="entityCode" id="entityCode" required/>
-        <label htmlFor="officeCode"></label>
-        <input type="number" name="officeCode" id="officeCode" required/>
-        <label htmlFor="digitControl"></label>
-        <input type="number" name="digitControl" id="digitControl" required/>
-        <label htmlFor="numberAccount"></label>
-        <input type="number" name="numberAccount" id="numberAccount" required/>
+      <p className="error">{error}</p>
+      <form className="create-account-form" onSubmit={handleSubmit}>
+        <fieldset>
+          <h3>Alias</h3>
+          <label htmlFor="alias">Escribe un nombre (alias) para tu nueva cuenta</label>
+          <input type="text" name="alias" id="alias" required/>
+        </fieldset>
+        <fieldset>
+          <h3>Banco</h3>
+          <label htmlFor="bankName">Banco</label>
+          <input type="text" name="bankName" id="bankName"/>
+        </fieldset>
+        <fieldset>
+          <h3>Número de cuenta</h3>
+          <label htmlFor="ibanCode"></label>
+          <input type="text" name="ibanCode" id="ibanCode" required pattern="[A-Z]{2}[0-9]{2}" placeholder="ES21"/>          
+          <label htmlFor="entityCode"></label>
+          <input type="text" name="entityCode" id="entityCode" required pattern="[0-9]{4}" placeholder="1234"/>
+          <label htmlFor="officeCode"></label>
+          <input type="text" name="officeCode" id="officeCode" required pattern="[0-9]{4}" placeholder="1234"/>
+          <label htmlFor="digitControl"></label>
+          <input type="text" name="digitControl" id="digitControl" required pattern="[0-9]{2}" placeholder="12"/>
+          <label htmlFor="numberAccount"></label>
+          <input type="text" name="numberAccount" id="numberAccount" required pattern="[0-9]{10}" placeholder="1234567890"/>
+        </fieldset>
         <button>Crear cuenta</button>
       </form>
     </section>
