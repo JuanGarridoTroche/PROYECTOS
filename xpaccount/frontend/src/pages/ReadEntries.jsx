2d89335f5691418e8c724c92d165b67bc68665a8
Juan Garrido Troche <j.garridotroche@gmail.com>
2023-04-04 23:16:53 +0200

    xpaccount, CREATE: Entries - componentizando los asientos bancarios

--- xpaccount/frontend/src/pages/ReadEntries.jsx
+++ xpaccount/frontend/src/pages/ReadEntries.jsx
@@ -11,6 +11,7 @@ import { TableHead } from "../components/TableHead";
 import {BarChart} from "../components/BarChart";
 import { UpdateEntry } from "../components/UpdateEntry";
 import { AccountData } from "../components/AccountData";
+import { Entries } from "../components/Entries";
 
 export const ReadEntries = () => {
   const { idAccount } = useParams();
@@ -97,25 +98,7 @@ export const ReadEntries = () => {
             <TableHead />
             {entries.reverse().map((entry) => {
               return (
-                <tbody key={entry.id} className="tbody-entries">
-                  <tr>
-                    <td>{entry.dateEntry}</td>
-                    <td>{entry.category}</td>
-                    <td>{entry.subcategory}</td>
-                    <td className={parseFloat(entry.amount) > 0 ? "numbers" : "numbers negative"}>
-                      {parseFloat(entry.amount).toFixed(2)} EUR
-                    </td>
-                    <td className={parseFloat(entry.total) > 0 ? "numbers" : "numbers negative"}>
-                        {parseFloat(entry.total).toFixed(2)} EUR
-                    </td>
-                    
-                    <td className="concept">{entry.concept}</td>
-                    <td className="comment">{entry.comment}</td>
-                    <td>
-                      <UpdateEntry entry={entry}/>
-                    </td>
-                  </tr>
-                </tbody>
+                <Entries entry={entry} key={entry.id}/>
               );
             })}
             <AddEntry
