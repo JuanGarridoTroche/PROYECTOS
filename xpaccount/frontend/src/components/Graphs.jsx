4ba35148c53c33719059717df300be8af37e4615
Juan Garrido Troche <j.garridotroche@gmail.com>
2023-04-07 23:58:16 +0200

    xpaccount, CREATE: componente Graphs. Creando datasets con los datos de la cuenta

--- xpaccount/frontend/src/components/Graphs.jsx
+++ xpaccount/frontend/src/components/Graphs.jsx
@@ -3,13 +3,14 @@ import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title,  Toolt
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);               
 import {Bar} from 'react-chartjs-2';
 import { useParams } from 'react-router-dom';
-import { readingAccountService } from '../services';
+import { readingAccountService, readEntriesByAccountService } from '../services';
 import { AuthContext } from '../context/AuthContext';
 
 export const Graphs = ()=> {
   const {idAccount} = useParams();
   const {token} = useContext(AuthContext);
   const [myAccount, setMyAccount] = useState({})
+  const [entries, setEntries] = useState([]);
 
   useEffect(()=>{
     const loadAccountData = async()=>{
@@ -18,11 +19,33 @@ export const Graphs = ()=> {
       
       setMyAccount(readingAccount);
 
+      // Leer todos los asientos de la cuenta idAccount
+      const readingEntries = await readEntriesByAccountService({idAccount, token});
+      console.log(readingEntries);
+      setEntries(readingEntries)
+      const allCategories = readingEntries.map((category) => {
+        return category.category; 
+      })
+      const ObjCategories = new Set(allCategories)
+      const categories = [...ObjCategories];
+      console.log(categories);
+      for(let i = 0; i < categories.length ; i++) {
+        
+      } 
+      const myDatasets = [
+        
+        {
+          id: '0',
+          label : categories[i]
+        }
+       ]
     }
     if(token) {loadAccountData()};
 
   },[])
 
+
+  const colors =  ['#001219', '#005f73', '#0a9396', '#94d2bd', '#e9d8a6', '#ee9b00', '#ca6702', '#bb3e03', '#ae2012', '#9b2226']
 
   const options = {
     plugins: {
@@ -49,19 +72,19 @@ export const Graphs = ()=> {
         id: 1,
         label: 'Alimentación',
         data: [312.5, 423.89, 234.17, 328.45],
-        backgroundColor: "rgb(255, 99, 132)",
+        backgroundColor: colors[0],
       },
       {
         id: 2,
         label: 'Transferencias',
         data: [122.5, 1338.9, 730.74, 928.12],
-        backgroundColor: "rgb(75, 192, 192)",
+        backgroundColor: colors[1],
       },
       {
         id: 3,
         label: 'Recibos',
         data: [212.5, 123.89, 201.11, 728.45],
-        backgroundColor: "rgb(53, 162, 235)",
+        backgroundColor: colors[2],
       }
     ]
   }
