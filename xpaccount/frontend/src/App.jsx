7b5539a0679112483809584b997a950f6257da34
Juan Garrido Troche <j.garridotroche@gmail.com>
2023-04-10 22:12:59 +0200

    xpaccount, FRONTEND FIXED: error en la página principal por haber generado mal lal ógica del useEffect

--- xpaccount/frontend/src/App.jsx
+++ xpaccount/frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import("./css/Main.css");
 import("./css/style.css");
 
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Home } from "./components/Home";
 import { Accounts } from "./pages/Accounts";
 import { CreateAccount } from "./pages/CreateAccount";
@@ -15,51 +15,58 @@ import { UserProfile } from "./pages/UserProfile";
 import { ValidateUSer } from "./pages/ValidateUser";
 import { ReadCategories } from "./pages/ReadCategories";
 import { ReadSubcategories } from "./pages/ReadSubcategories";
-import { BarChart } from "./components/BarChart";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "./context/AuthContext";
-import { getAccountsUserService, readEntriesByAccountService } from "./services";
+import {
+  getAccountsUserService,
+  readEntriesByAccountService,
+} from "./services";
 import { Graphs } from "./components/Graphs";
-
 
 function App() {
   const [balance, setBalance] = useState([]);
-  const {token} = useContext(AuthContext);
+  const { token } = useContext(AuthContext);
   const [myAccounts, setMyAccounts] = useState([]);
-  
+
   // Carga los datos de todas las cuentas junto con sus asientos bancarios y calcula el saldo de cada cuenta
-  useEffect(()=> {
+  useEffect(() => {
     const myAccountBalance = [];
-    const getBalanceData = async () => {   
-         
+    const getBalanceData = async () => {
       try {
         // Si el usuario no está logueado no puede eacceder a esta página
-        if(token) {        
-        // Obtenemos todas las cuentas bancarias creadas por el usuario logueado
-        const getAccounts = await getAccountsUserService(token);
-        // console.log("Conseguir cuentas: ", getAccounts);
-        if (getAccounts.length > 0) {         
-          setMyAccounts(getAccounts);                   
-          // Obtener todos los asientos bancarios de cada cuenta creada        
-          for(let i=0;i < getAccounts.length;i++) {
-            // console.log("bucle for: ", getAccounts[i].id);
-            const idAccount = getAccounts[i].id;
-            const readingEntries = await readEntriesByAccountService({idAccount, token })
-            // console.log(readingEntries); 
-            const calculatingBalance = readingEntries.reduce((accumulator, current) => accumulator + parseFloat(current.amount), 0)      
-            
-            myAccountBalance.push({id: getAccounts[i].id, balance: calculatingBalance})          
-          }
-          setBalance(myAccountBalance);
-        }
-      };
+        if (token) {
+          // Obtenemos todas las cuentas bancarias creadas por el usuario logueado
+          const getAccounts = await getAccountsUserService(token);
 
+          if (getAccounts.length > 0) {
+            setMyAccounts(getAccounts);
+            // Obtener todos los asientos bancarios de cada cuenta creada
+            for (let i = 0; i < getAccounts.length; i++) {
+              const idAccount = getAccounts[i].id;
+              const readingEntries = await readEntriesByAccountService({
+                idAccount,
+                token,
+              });
+              const calculatingBalance = readingEntries.reduce(
+                (accumulator, current) =>
+                  accumulator + parseFloat(current.amount),
+                0
+              );
+
+              myAccountBalance.push({
+                id: getAccounts[i].id,
+                balance: calculatingBalance,
+              });
+            }
+            setBalance(myAccountBalance);
+          }
+        }
       } catch (error) {
         alert(error.message);
       }
     };
     getBalanceData();
-  },[token])
+  }, [token]);
 
   return (
     <>
@@ -85,7 +92,7 @@ function App() {
           <Route path="/user/profile" element={<UserProfile />} />
 
           {/* Mostrar las cuentas de un usuario registrado */}
-          <Route path="/accounts" element={<Accounts balance={balance}/>} />
+          <Route path="/accounts" element={<Accounts balance={balance} />} />
 
           {/* Crear una cuenta nueva */}
           <Route path="/accounts/create" element={<CreateAccount />} />
@@ -97,13 +104,16 @@ function App() {
           />
 
           {/* Leer las subcategorías de una categoría de la cuenta */}
-          <Route path="/account/:idAccount/category/:idCategory" element={<ReadSubcategories/>}/>
+          <Route
+            path="/account/:idAccount/category/:idCategory"
+            element={<ReadSubcategories />}
+          />
 
           {/* Acceder a los asientos bancarios de una cuenta */}
-          <Route path="/account/:idAccount" element={<ReadEntries/>} />
+          <Route path="/account/:idAccount" element={<ReadEntries />} />
 
           {/* Mostrar los gráficos de los asientos bancarios de una cuenta */}
-          <Route path="/account/:idAccount/graphs" element={<Graphs/>} />
+          <Route path="/account/:idAccount/graphs" element={<Graphs />} />
 
           {/* Página no encontrada */}
           <Route path="*" element={<NotFound />} />
