2a9e4409514578c9dec8637f190feca78deb2b8a
Juan Garrido Troche <j.garridotroche@gmail.com>
2023-04-24 16:46:39 +0200

    aprendiendo typescript: Proyecto gestión de gastos

--- gastos-typescript/public/script/main.js
+++ gastos-typescript/public/script/main.js
@@ -1,4 +1,9 @@
-import { Account } from "./Account.js";
+import { Account, Entry, CategoryEnum } from "./Account.js";
 console.log("Cargando typescript...⏱️");
 var account = new Account();
+console.log(account);
+var expenseEjemplo = new Entry("Ejemplo de gasto", 100, CategoryEnum.expense);
+var incomeEjemplo = new Entry("Ejemplo de gasto", 150, CategoryEnum.income);
+account.addEntry(expenseEjemplo);
+account.addEntry(incomeEjemplo);
 console.log(account);
--- gastos-typescript/src/main.ts
+++ gastos-typescript/src/main.ts
@@ -1,7 +1,14 @@
-import { Account } from "./Account.js";
+import { Account, Entry, CategoryEnum } from "./Account.js";
 
 console.log("Cargando typescript...⏱️");
 
 const account = new Account();
 
+console.log(account);
+
+const expenseEjemplo = new Entry("Ejemplo de gasto", 100, CategoryEnum.expense);
+const incomeEjemplo = new Entry("Ejemplo de gasto", 150, CategoryEnum.income);
+
+account.addEntry(expenseEjemplo);
+account.addEntry(incomeEjemplo);
 console.log(account);
