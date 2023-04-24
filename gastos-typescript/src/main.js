ba373c7a73850b862dc169b3c943e49b3ed2b847
Juan Garrido Troche <j.garridotroche@gmail.com>
2023-04-24 22:52:43 +0200

    aprendiendo typescript: Proyecto gestión de gastos

--- gastos-typescript/src/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Account_js_1 = require("./Account.js");
-console.log("Cargando typescript...⏱️");
-var account = new Account_js_1.Account();
-console.log(account);
--- gastos-typescript/src/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getRandomId = void 0;
-// Obtener un número al azar como Id del nuevo asiento bancario
-function getRandomId() {
-    return Math.floor(Math.random() * Date.now());
-}
-exports.getRandomId = getRandomId;
