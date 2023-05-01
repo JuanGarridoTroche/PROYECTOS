7f831dfde744337f48673cb3e432b748b91c48a5
Juan Garrido Troche <j.garridotroche@gmail.com>
2023-05-01 23:56:56 +0200

    proyecto carrito de la compra: Frontend creación de árbol de carpetas

--- carrito/frontend/scripts/main.js
+++ carrito/frontend/scripts/main.js
@@ -9,6 +9,15 @@ const items = document.querySelector('.items');
 function getData() {
   fetch(`${serverURL}${itemsPath}`)
     .then((response) => response.json())
-    .then((data) => console.log(data))
-    // .then((data) => printData(data))
+    // .then((data) => console.log(data))
+    .then((data) => printData(data))
+}
+
+function printData(data) {
+  const itemContainer = document.createElement('div');
+  itemContainer.className = 'row';
+  data.forEach(item => {
+    itemContainer.innerHTML += createDomElement(item);
+    items.append(itemContainer)
+  });
 }
