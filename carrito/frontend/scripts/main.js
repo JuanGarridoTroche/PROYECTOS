b24ee4a31ccf532dba6bf3df2786d013db338e7b
Juan Garrido Troche <j.garridotroche@gmail.com>
2023-05-02 00:08:52 +0200

    proyecto carrito de la compra: Frontend cargando los datos desde items.json

--- carrito/frontend/scripts/main.js
+++ carrito/frontend/scripts/main.js
@@ -5,6 +5,7 @@ const imagesPath = 'img/';
 window.onload = getData();
 
 const items = document.querySelector('.items');
+console.log('🔥: items', items);
 
 function getData() {
   fetch(`${serverURL}${itemsPath}`)
@@ -20,4 +21,20 @@ function printData(data) {
     itemContainer.innerHTML += createDomElement(item);
     items.append(itemContainer)
   });
+}
+
+function createDomElement(item) {
+  const itemHTML = `
+    <div class="col-12 col-md-6">
+      <div class="item shadow mb-4">
+        <h3 class="item-title">${item.title}</h3>
+        <img class="item-image" src=${imagesPath}${item.image}>
+
+        <div class="item-details">
+          <h4 class="item-price">${item.price}€</h4>
+          <button class="item-button btn btn-primary addToCart">AÑADIR AL CARRITO</button>
+        </div>
+      </div>
+    </div>`
+    return itemHTML;
 }
