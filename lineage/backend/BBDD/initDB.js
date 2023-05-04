58f6f782643a357e6e95ea939baad78f7490ca93
Juan Garrido Troche <j.garridotroche@gmail.com>
2023-05-04 22:52:43 +0200

    creando nuevo proyecto: Lineage, creación de base de datos, Backend: getConnection y initDB

---
+++ lineage/backend/BBDD/initDB.js
@@ -0,0 +1,108 @@
+"use strict";
+require("dotenv").config();
+
+// Importamos la función que retorna una conexión libre con la BBDD.
+const getConnection = require("./getConnection");
+
+const bcrypt = require("bcrypt");
+
+const initDB = async () => {
+  let connection;
+
+  try {
+    // Intentamos obtener una conexión de las 10 conexiones libres que tenemos.
+    connection = await getConnection();
+
+    console.log("Borrando tablas...");
+
+    await connection.query("DROP TABLE IF EXISTS descends");
+    await connection.query("DROP TABLE IF EXISTS items");
+    await connection.query("DROP TABLE IF EXISTS lineages");
+    await connection.query("DROP TABLE IF EXISTS users");
+
+    console.log("Creando tablas...");
+
+    await connection.query(`
+            CREATE TABLE IF NOT EXISTS users (
+              id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+              email VARCHAR(100) UNIQUE NOT NULL,
+              password VARCHAR(100) NOT NULL,
+              role ENUM('admin', 'mod', 'user') DEFAULT 'user' NOT NULL,
+              first_name VARCHAR(100) NOT NULL,
+              last_name1 VARCHAR(100) NOT NULL,
+              last_name2 VARCHAR(100) NOT NULL,
+              registrationCode VARCHAR(100),
+              recoverPassCode VARCHAR(20),
+              active BOOLEAN DEFAULT false,
+              createdAt TIMESTAMP NOT NULL,
+              modifiedAt TIMESTAMP
+            )
+        `);
+    console.log("tabla users...");
+
+    await connection.query(`
+      CREATE TABLE IF NOT EXISTS lineages (
+        id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+        lineage VARCHAR(100) UNIQUE NOT NULL,
+        description VARCHAR(900),	
+        shield VARCHAR(100),
+        createdAt TIMESTAMP NOT NULL,
+        modifiedAt TIMESTAMP
+      )
+    `);
+
+    console.log("tabla lineages...");
+
+    await connection.query(`
+      CREATE TABLE IF NOT EXISTS items (
+        id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+        id_lineage INT UNSIGNED NOT NULL,
+        FOREIGN KEY (id_lineage) REFERENCES lineages(id),
+        name VARCHAR(100) NOT NULL,
+        createdAt TIMESTAMP NOT NULL,
+        modifiedAt TIMESTAMP
+      )
+    `);
+
+    console.log("tabla items...");
+
+    await connection.query(`
+      CREATE TABLE IF NOT EXISTS descends (
+        id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+        id_lineage INT UNSIGNED NOT NULL,
+        FOREIGN KEY (id_lineage) REFERENCES lineages(id),
+        id_user INT UNSIGNED NOT NULL,
+        FOREIGN KEY (id_user) REFERENCES users(id),
+        createdAt TIMESTAMP NOT NULL,
+        modifiedAt TIMESTAMP
+      )
+    `);
+
+    console.log("tabla descends...");
+    console.log("¡Tablas creadas!");
+
+    // Encriptamos la contraseña del admin.
+    const adminPass = await bcrypt.hash(process.env.ADMIN_PASS, 10);
+
+    // Insertamos el usuario administrador.
+    await connection.query(
+      `
+                INSERT INTO users (email, password, role, first_name, last_name1, last_name2, active, createdAt)
+                VALUES ('juan@darthvader.es', ?, 'admin', 'Administrador', 'admin', 'admin', true, ?)
+            `,
+      [adminPass, new Date()]
+    );
+
+    console.log("¡Usuario administrador creado!");
+  } catch (err) {
+    console.error(err);
+  } finally {
+    if (connection) connection.release();
+
+    // Cerramos el proceso.
+    process.exit();
+  }
+};
+
+// Ejecutamos la función initDB.
+initDB();
--- lineage/backend/server.js
+++ lineage/backend/server.js
@@ -1,10 +1,24 @@
+
+const morgan = require('morgan');
 const express = require('express');
 require('dotenv').config();
 const {BACK_PORT, BACK_HOST} = process.env;
 
 const app = express();
 
+//MIDDLEWARE - Logger morgan: muestra cualquier petición al servidor a través de nuestro terminal de node
+app.use(morgan('dev'));
 
+
+
+/*
+ * ###########################
+ * ## Middleware de /users  ##
+ * ###########################
+ */
+
+
+app.get('/users');
 
 // SERVER LISTENING
 app.listen(BACK_PORT, () => {
