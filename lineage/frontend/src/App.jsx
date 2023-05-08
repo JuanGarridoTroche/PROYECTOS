585186a133ac3667b6c4c1bc5525fcd232c3108b
Juan Garrido Troche <j.garridotroche@gmail.com>
2023-05-08 21:17:18 +0200

    Comenzando aplicación Lineage con REACT + Vite + SWC(Speedy Web Compiler)

--- lineage/frontend/src/App.jsx
+++ lineage/frontend/src/App.jsx
@@ -1,11 +1,16 @@
-import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
 
-function App() {  
+import ('./App.css')
 
+
+function App() {
   return (
-    <>
-      <p>Hola!</p>
-    </>
+    <section className='App'>
+      <TwitterFollowCard account='midudev' name='Miguel Durán' isFollowing={true} avatar='midudev'/>
+      <TwitterFollowCard account='grddev' name='Juan Garrido Troche' isFollowing={false} avatar='pheralb'/>
+      <TwitterFollowCard account='elonmusk' name='Elon Musk' isFollowing={true} avatar='elonmusk'/>
+      <TwitterFollowCard account='vanderHarst' name='Vander Harst' isFollowing={true} avatar='vanderHarst'/>
+    </section>
   )
 }
 
