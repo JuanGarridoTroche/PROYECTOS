ead43d82545bd0d3436c3a9458e660f1020debfc
Juan Garrido Troche <j.garridotroche@gmail.com>
2023-05-09 00:39:13 +0200

    Lineage Frontend: Creación estructura del menú: Brand + navbar + login

--- lineage/frontend/src/App.jsx
+++ lineage/frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import ('./App.css')
 import { Header } from './components/header/Header'
+import { Main } from './components/main/Main'
+import { Footer } from './components/footer/Footer'
 
 function App() {
   return (
     <>
       <Header/>
+      <Main/>
+      <Footer/>
     </>
   )
 }
---
+++ lineage/frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import ('../../css/Footer.css')
+export const Footer = ()=> {
+  return (
+    <footer className="foot-info">
+      <section className="foot-madeIn">
+        © 2023 - Aplicación realizada por @grddev 
+      </section>
+    </footer>
+  )
+}
