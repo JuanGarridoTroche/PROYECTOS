eb5499afcbfcbbe87ce73756f52a57982b2989b2
Juan Garrido Troche <j.garridotroche@gmail.com>
2023-05-09 23:58:19 +0200

    Lineage Frontend, CREATE: arreglando css

--- lineage/frontend/src/components/header/Brand.jsx
+++ lineage/frontend/src/components/header/Brand.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom"
 
 
 export const Brand = ()=> {
+  
   return (
     <figure className="menu-header-figure">
-      <img src="/lineage-logo.png" alt="Logo de Lineage" className="menu-header-figureLogo"/>
+      <Link to="/">
+        <img src="/lineage-logo.png" alt="Logo de Lineage" className="menu-header-figureLogo"/>
+      </Link>
       <p  className="menu-header-figureName">Linea<span className="g">g</span>e</p>
     </figure>
   )
