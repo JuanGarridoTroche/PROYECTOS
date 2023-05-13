c2a9bbf541828c0756613a2db1668a49d4a12f0c
Juan Garrido Troche <j.garridotroche@gmail.com>
2023-05-14 00:00:17 +0200

    Lineage, FRONTEND, CREATE: Register.jsx

--- lineage/frontend/src/pages/RegisterPage.jsx
+++ lineage/frontend/src/pages/RegisterPage.jsx
@@ -1,14 +1,32 @@
-import ('../css/Page.css')
-export const RegisterPage = ()=> {
+import("../css/Page.css");
+export const RegisterPage = () => {
   return (
-    <section className='section-main'>
+    <section className="section-main">
       <details className="section-main__details details">
         <summary className="details__summary">Registro de usuario</summary>
         <p className="details__p">
-          Regístrate en nuestra aplicación para tener acceso a todos los servicios de Linea<span className="g">g</span>e
+          Regístrate en nuestra aplicación para tener acceso a todos los
+          servicios de Linea<span className="g">g</span>e
         </p>
       </details>
-
+      <form className="section-main__form form">
+        <fieldset className="form__fieldset-first-name fieldset-first-name">
+          <h3 className="fieldset-first-name__h3">Nombre</h3>
+          <label htmlFor="first_name" className="fieldset-first-name__label--summary">
+            Escribe tu nombre
+          </label>
+          <input
+            type="text"
+            name="first_name"
+            id="first_name"
+            autoComplete="on"
+            placeholder="Nombre"
+            required
+            className="fieldset-first-name__input"
+          />
+        </fieldset>
+        <fieldset></fieldset>
+      </form>
     </section>
-  )
-}
+  );
+};
