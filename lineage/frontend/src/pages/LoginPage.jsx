356127f2cb49030b32fc9610aeec476a767448c9
Juan Garrido Troche <j.garridotroche@gmail.com>
2023-05-14 13:44:48 +0200

    Lineage, FRONTEND, CREATE: Register.jsx

--- lineage/frontend/src/pages/LoginPage.jsx
+++ lineage/frontend/src/pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import("../css/Page.css");
+import("../css/LoginPage.css");
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { loginUserService } from "../services";
--- lineage/frontend/src/pages/RegisterPage.jsx
+++ lineage/frontend/src/pages/RegisterPage.jsx
@@ -1,5 +1,11 @@
-import("../css/Page.css");
+import { useState } from "react";
+import { Modal } from "../../../../xpaccount/frontend/src/components/Modal";
+import { Message } from "../components/main/Message";
+
+import("../css/RegisterPage.css");
 export const RegisterPage = () => {
+  const[showModal, setShowModal] = useState("");
+  const [error, setError] = useState("");
   return (
     <section className="section-main">
       <details className="section-main__details details">
@@ -9,38 +15,78 @@ export const RegisterPage = () => {
           servicios de Linea<span className="g">g</span>e
         </p>
       </details>
-      <form className="section-main__form form">
-        <fieldset className="form__fieldset-first-name fieldset-first-name">
-          <h3 className="fieldset-first-name__h3">Nombre</h3>
-          <label htmlFor="first_name" className="fieldset-first-name__label--summary">
-            Escribe tu nombre
-          </label>
-          <input
-            type="text"
-            name="first_name"
-            id="first_name"
-            autoComplete="on"
-            placeholder="Nombre"
-            required
-            className="fieldset-first-name__input"
-          />
-        </fieldset>
-        <fieldset className="form__fieldset-last-name1 fieldset-last-name1">
-          <h3 className="fieldset-last-name1__h3">Nombre</h3>
-          <label htmlFor="first_name" className="fieldset-last-name1__label--summary">
-            Escribe tu nombre
-          </label>
-          <input
-            type="text"
-            name="first_name"
-            id="first_name"
-            autoComplete="on"
-            placeholder="Nombre"
-            required
-            className="fieldset-first-name__input"
-          />
-        </fieldset>
-      </form>
+      <section className="section-main__section--content section--content">
+      {error ? <Message message={error} type="error"/> : null}
+        <form className="section-main__form form">
+          <fieldset className="form__fieldset form__fieldset--first_name fieldset--first-name">
+            <h3 className="h3 fieldset__h3--first_name">Nombre</h3>
+            <label htmlFor="first_name" className="label fieldset__label--first-name">
+              Escribe tu nombre
+            </label>
+            <input
+              type="text"
+              name="first_name"
+              id="first_name"
+              autoComplete="on"
+              placeholder="Nombre"
+              required
+              className="input fieldset__input fieldset__input--first-name"
+            />
+          </fieldset>
+          <fieldset className="form__fieldset form__fieldset--last_name1 fieldset--last-name1">
+            <h3 className="h3 fieldset__h3--last_name1">Primer apellido</h3>
+            <label htmlFor="last_name1" className="label fieldset__label--last-name1">
+              Escribe tu primer apellido
+            </label>
+            <input
+              type="text"
+              name="last_name1"
+              id="last_name1"
+              autoComplete="on"
+              placeholder="Primer apellido"
+              required
+              className="input fieldset__input fieldset__input--last-name1"
+            />
+          </fieldset>
+          <fieldset className="form__fieldset form__fieldset--last_name2 fieldset--last-name2">
+            <h3 className="h3 fieldset__h3--last_name2">Segundo apellido</h3>
+            <label htmlFor="last_name2" className="label fieldset__label--last-name2">
+              Escribe tu segundo apellido
+            </label>
+            <input
+              type="text"
+              name="last_name2"
+              id="last_name2"
+              autoComplete="on"
+              placeholder="Segundo apellido"
+              className="input fieldset__input fieldset__input--last-name2"
+              required
+            />          
+          </fieldset>
+          <fieldset className="form__fieldset form__fieldset--email fieldset--email">
+            <h3 className="h3 fieldset__h3--email">Correo electrónico</h3>
+            <label htmlFor="email" className="label fieldset__label--email">
+              Escribe tu dirección de correo electrónico
+            </label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              autoComplete="on"
+              placeholder="correo@electronico.com"
+              required
+              className="input fieldset__input fieldset__input--email"
+            />
+          </fieldset>
+          <button className="section-main__form__button section-main__button button">Registrar</button>
+        </form>
+      </section>
+      {showModal && (
+        <Modal setShowModal={setShowModal}>
+          Usuario creado!
+          Revisa tu correo para activar la cuenta
+        </Modal>
+      )}
     </section>
   );
 };
