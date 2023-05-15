f1bc9ec5559962f9ffe16ca52d05568102a75ef1
Juan Garrido Troche <j.garridotroche@gmail.com>
2023-05-15 02:07:27 +0200

    Lineage, FRONTEND, CREATE: RegisterPage.jsx, queda pendiente validar el correo

--- lineage/frontend/src/pages/RegisterPage.jsx
+++ lineage/frontend/src/pages/RegisterPage.jsx
@@ -2,11 +2,33 @@ import { useState } from "react";
 import { Modal } from "../../../../xpaccount/frontend/src/components/Modal";
 import { Message } from "../components/main/Message";
 import { Link } from "react-router-dom";
+import { registerUserService } from "../services";
 
 import("../css/RegisterPage.css");
 export const RegisterPage = () => {
   const[showModal, setShowModal] = useState("");
   const [error, setError] = useState("");
+  const [first_name, setFirstName] = useState("");
+  const [last_name1, setLastName1] = useState("");
+  const [last_name2, setLastName2] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+
+  const handleSubmit = async(e)=> {
+    const newUser = { email, password, first_name, last_name1, last_name2};
+    
+    e.preventDefault();
+    setError("");
+    try {
+      await registerUserService(newUser);
+      setShowModal(true);
+      e.target.reset;
+    } catch (err) {
+      setError(error.message);
+    }
+  }
+
   return (
     <section className="section-main">
       <details className="section-main__details details">
@@ -18,7 +40,38 @@ export const RegisterPage = () => {
       </details>
       <section className="section-main__section--content section--content">
       {error ? <Message message={error} type="error"/> : null}
-        <form className="section-main__form form">
+        <form className="section-main__form form" onSubmit={handleSubmit}>
+          <fieldset className="form__fieldset form__fieldset--email fieldset--email">
+            <h3 className="h3 fieldset__h3--email">Correo electrónico</h3>
+            <label htmlFor="email" className="label fieldset__label--email">
+              Escribe tu dirección de correo electrónico
+            </label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              autoComplete="on"
+              placeholder="correo@electronico.com"
+              required
+              className="input fieldset__input fieldset__input--email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </fieldset>
+          <fieldset className="form__fieldset form__fieldset--password fieldset--password">
+            <h3 className="h3 fieldset__h3--password">Contraseña</h3>
+            <label htmlFor="password" className="label fieldset__label--password">
+              Escribe tu contraseña. Debe tener al menos 8 caracteres y contener al menos un caracter en mayúsculas, minúsculas, un número y un caracter especial
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              autoComplete="on"
+              required
+              className="input fieldset__input fieldset__input--password"
+              onChange={(e)=> setPassword(e.target.value)}
+            />
+          </fieldset>
           <fieldset className="form__fieldset form__fieldset--first_name fieldset--first-name">
             <h3 className="h3 fieldset__h3--first_name">Nombre</h3>
             <label htmlFor="first_name" className="label fieldset__label--first-name">
@@ -32,6 +85,7 @@ export const RegisterPage = () => {
               placeholder="Nombre"
               required
               className="input fieldset__input fieldset__input--first-name"
+              onChange={(e)=> setFirstName(e.target.value)}
             />
           </fieldset>
           <fieldset className="form__fieldset form__fieldset--last_name1 fieldset--last-name1">
@@ -47,6 +101,7 @@ export const RegisterPage = () => {
               placeholder="Primer apellido"
               required
               className="input fieldset__input fieldset__input--last-name1"
+              onChange={(e) => setLastName1(e.target.value)}
             />
           </fieldset>
           <fieldset className="form__fieldset form__fieldset--last_name2 fieldset--last-name2">
@@ -62,22 +117,8 @@ export const RegisterPage = () => {
               placeholder="Segundo apellido"
               className="input fieldset__input fieldset__input--last-name2"
               required
+              onChange={(e) => setLastName2(e.target.value)}
             />          
-          </fieldset>
-          <fieldset className="form__fieldset form__fieldset--email fieldset--email">
-            <h3 className="h3 fieldset__h3--email">Correo electrónico</h3>
-            <label htmlFor="email" className="label fieldset__label--email">
-              Escribe tu dirección de correo electrónico
-            </label>
-            <input
-              type="text"
-              name="email"
-              id="email"
-              autoComplete="on"
-              placeholder="correo@electronico.com"
-              required
-              className="input fieldset__input fieldset__input--email"
-            />
           </fieldset>
           <button className="section-main__form__button section-main__button button">Registrar</button>
           <p className="section-main__form__p--login p--login">¿Ya tienes una cuenta? <Link to={"/login"}>iniciar sesión</Link></p>
--- lineage/frontend/src/services/index.js
+++ lineage/frontend/src/services/index.js
@@ -1,20 +1,20 @@
 // Carga de la lista de usuarios registrados
-export const loadUsersService = async ()=> {
+export const loadUsersService = async () => {
   const response = await fetch(
-    `${import.meta.env.VITE_BACKEND_URL}:${import.meta.env.VITE_BACKEND_PORT}/users/list`
+    `${import.meta.env.VITE_BACKEND_URL}:${
+      import.meta.env.VITE_BACKEND_PORT
+    }/users/list`
   );
-  
-  // Desestructuramos el json que nos devuelve
- const json = await response.json();
 
-  if(!response.ok) {
+  // Desestructuramos el json que nos devuelve
+  const json = await response.json();
+
+  if (!response.ok) {
     throw new Error(json.message);
   }
-  
+
   return json.data;
-}
-
-
+};
 
 // Servicio de login de usuario. Devuelve un objeto con {token: ''}
 export const loginUserService = async (email, password) => {
@@ -32,9 +32,31 @@ export const loginUserService = async (email, password) => {
   );
   const json = await response.json();
 
-  if(!response.ok) {
+  if (!response.ok) {
     throw new Error(json.message);
   }
   // console.log(json.data);
+  return json.data;
+};
+
+// Registro de un nuevo usuario
+export const registerUserService = async (newUser) => {
+
+  const response = await fetch(
+    `${import.meta.env.VITE_BACKEND_URL}:${
+      import.meta.env.VITE_BACKEND_PORT
+    }/users/register`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    }
+  );
+  const json = response.json();
+  if(!response.ok) {
+    throw new Error(json.message);
+  }
   return json.data;
 };
