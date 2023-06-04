9ad49ba6c8f307be4f45cc038ff280cb1f100974
Juan Garrido Troche <j.garridotroche@gmail.com>
2023-06-05 01:15:13 +0200

    Seco-fam, CREATE: empezamos de ceero con React

---
+++ seco-fam/src/pages/Main.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import ("../css/Main.css");
+import data from "../assets/lineages.json";
+
+
+
+export const Main = ()=> {
+  const [pwd, setPwd] = useState("");
+
+  const handleSubmit = (e)=> {
+    e.preventDefault();
+    
+    try {
+      const checkingPass = data.find(e => e.pass === pwd.toString())
+      if(!checkingPass){
+        throw new Error("Contraseña incorrecta")
+      }
+
+      console.log("contraseña correcta para ",checkingPass.lineage);
+      
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+  return(
+    <main className="main">      
+      <form onSubmit={handleSubmit} action="#" method="get" className="main__form">
+        <label htmlFor="pass" className="main__pass--label">Escribe tu contraseña </label>
+        <input type="password" id="pass" className="main__pass" placeholder="Introduzca contraseña" autoComplete="on" onChange={(e)=> {setPwd(e.target.value)}}/>
+        <button className="main__button" hidden>Enviar</button>
+      </form>
+  </main>
+  )
+}
