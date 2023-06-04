75190a2d5cf93f095d02b0123f67616202b8d83c
Juan Garrido Troche <j.garridotroche@gmail.com>
2023-06-04 22:41:42 +0200

    Seco, CREATE: main.js

---
+++ seco-fam/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const input = document.getElementById("pass");
+const btn = document.querySelector(".main__button")
+
+btn.addEventListener("click", (e)=> {
+  e.preventDefault();
+  console.log();
+})
+
+
