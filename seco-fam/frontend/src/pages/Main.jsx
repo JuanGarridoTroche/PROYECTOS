3bbf67184374dd11d46d4d1de7cd2a732d546413
Juan Garrido Troche <j.garridotroche@gmail.com>
2023-06-06 14:08:27 +0200

    seco-fam, CREATE: gestión de errores

--- seco-fam/frontend/src/pages/Main.jsx
+++ seco-fam/frontend/src/pages/Main.jsx
@@ -15,20 +15,22 @@ export const Main = ()=> {
       const checkingPass = data.find(e => e.pass === pwd.toString());
       
       if(!checkingPass){
-        throw new Error("Contraseña incorrecta")
+              
+        throw new Error("Contraseña incorrecta");
       }
 
       console.log("contraseña correcta para ",checkingPass.lineage);
       
     } catch (err) {
-      console.error(err.message);
+      setError(err.message);
     }
   }
   return(
     <main className="main">      
+    {error ? <p className="main__error">{error}</p> : null}
       <form onSubmit={handleSubmit} action="#" method="get" className="main__form">
         {/* <label htmlFor="pass" className="main__pass--label">Escribe tu contraseña </label> */}
-        <input type="password" id="pass" className="main__pass" placeholder="Introduzca su contraseña" autoComplete="on" onChange={(e)=> {setPwd(e.target.value)}}/>
+        <input type="password" id="pass" className="main__pass" placeholder="Introduzca su contraseña" autoComplete="on" onChange={(e)=> {setError(""); setPwd(e.target.value)}}/>
         <button className="main__button" hidden>Enviar</button>
       </form>
   </main>
