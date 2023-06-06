189d75f1aed2f834f132333daaba49ce85f02339
Juan Garrido Troche <j.garridotroche@gmail.com>
2023-06-07 00:21:48 +0200

    seco-fam, BACKEND, CREATE: creación de encriptado de contraseñas

--- seco-fam/backend/controllers/users/login.js
+++ seco-fam/backend/controllers/users/login.js
@@ -3,22 +3,27 @@ const bcrypt = require('bcrypt');
 const data = require("../../assets/lineages.json")
 
 const login = async (req, res, next) => {
-  const {password} = req.body;
+  const {lineage, password} = req.body;
 
-  // Encriptamos la contraseña
-  const hashPass = await bcrypt.hash(password, 10);
-  console.log(hashPass);
   try {
-    console.log(password);
-    const checkingPass = data.find(e => e.pass === password.toString());
-    if(!checkingPass){              
-      throw generateError("Contraseña incorrecta", 403);
+
+    // Comprobamos que la familia introducida existe
+    const checkingLineage = data.find(e => e.lineage === lineage);
+    if(!checkingLineage) {
+      throw generateError("Datos y/o contraseña incorrecto/s", 400)
+    }
+
+    //Comprobamos que la contraseña es válida
+    const validPassword = await bcrypt.compare(password, checkingLineage.password);
+    console.log(validPassword);
+   
+    if(!validPassword){              
+      throw generateError("Datos y/o contraseña incorrecto/s", 400);
     }
 
     res.send({
       status: "Ok",
-      message: password,
-      body: data,
+      message: `login realizado con éxito por '${checkingLineage.lineage}'`,
     })
     
   } catch (err) {
