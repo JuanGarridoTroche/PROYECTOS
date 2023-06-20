2448b8a8fd4b0173b82644b49042540c85d78cd1
Juan Garrido Troche <j.garridotroche@gmail.com>
2023-06-20 20:38:03 +0200

    seco-fam, Backend: Creando el css según el rol del usuario logueado

--- 10-seco-fam/backend/assets/queries/getAllFamilies.js
+++ 10-seco-fam/backend/assets/queries/getAllFamilies.js
@@ -4,7 +4,7 @@ const getAllFamilies = () => {
   let lineage = [];
   try {
     for(let family of data) {
-      lineage.push({id: family.id, lineage: family.lineage});      
+      lineage.push({id: family.id, lineage: family.lineage, url: family.url, logo: family.logo});      
     }
     lineage.pop();
     return lineage;
