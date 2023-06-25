56306cca83798cb2e6a7d3eeb3b7aa94d7688c9c
Juan Garrido Troche <j.garridotroche@gmail.com>
2023-06-25 22:06:28 +0200

    10-fam-seco, backend: create subir pdf con admin

--- 10-seco-fam/backend/controllers/users/updateJSON.js
+++ 10-seco-fam/backend/controllers/users/updateJSON.js
@@ -1,26 +1,36 @@
 const {generateError} = require("../../helpers")
+const data = require("../../assets/lineages.final.json");
+const myData = structuredClone(data);  
 
 const updateJSON = async (req, res, next) => {
-  let data=[];
-  try {   
-    
-    if(req.files) {
-      const {pdf} = req.files;
+  const {lineage} = req.body;
+  console.log("Familia: ", lineage);
   
-      if(!pdf.name || !pdf.data || pdf.mimetype !== 'application/pdf') {
-        throw generateError("Introduzca un fichero pdf válido", 400)
-      }
-
-      data = [pdf];
-
+  try {   
+    if(!req.files) {
+      throw generateError("No hay un fichero pdf",400)
     }
     
     
+    const {pdf} = req.files;  
+    if(!pdf.name || !pdf.data || pdf.mimetype !== 'application/pdf') {
+      throw generateError("Introduzca un fichero pdf válido", 400)
+    }
+
+    myData.map((item, index)=> {
+      console.log("Familia recorriendo el array: ", item.lineage);
+      if(item.lineage === lineage) {
+        // myData[index].pdf = [...item.pdf, pdf.name] 
+        myData[index].pdf.push(pdf.name);
+      }
+    })
+
+   console.log(myData);
     
     res.send({
       status: "Ok",
       message: `datos subidos al json`, 
-        data,  
+      myData,  
     })
     
   } catch (err) {
