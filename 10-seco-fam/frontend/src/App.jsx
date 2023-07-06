a5312bed974006fb9652487265b291df06906127
Juan Garrido Troche <j.garridotroche@gmail.com>
2023-07-06 21:42:04 +0200

    4fam, feat frontend: NotFound,jsx y css

--- 10-seco-fam/frontend/src/App.jsx
+++ 10-seco-fam/frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import { Routes, Route} from "react-router-dom";
 import { Header } from "./components/Header";
 import { Login } from "./pages/Login";
-import { useContext, useEffect} from "react";
+import { useContext} from "react";
 import { AuthContext } from "./contexts/AuthContext";
 import { NotFound } from "./pages/NotFound";
 
 import ("./css/App.css");
 
 function App() {
-  const {token, user} = useContext(AuthContext);
+  const {token} = useContext(AuthContext);
 
 
 
