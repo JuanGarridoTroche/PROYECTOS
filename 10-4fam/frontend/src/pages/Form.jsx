d23da884dd4af6f39d613401c4713969688ddbdd
Juan Garrido Troche <j.garridotroche@gmail.com>
2023-07-13 18:29:22 +0200

    4fam bakend/frontend feat: añadido del correo electrónico de contacto en el formulario de contacto

--- 10-4fam/frontend/src/pages/Form.jsx
+++ 10-4fam/frontend/src/pages/Form.jsx
@@ -11,6 +11,7 @@ export const Form = ()=> {
   const [name, setName] = useState("");
   const[subject, setSubject] = useState("");
   const [text, setText] = useState("");
+  const [emailContact, setEmailContact] = useState("");
   const navigate = useNavigate();
   const {token} = useContext(AuthContext);
   const [showModal, setShowModal] = useState(false); 
@@ -25,12 +26,13 @@ export const Form = ()=> {
         navigate("/");
       }
       
-      if(!name || !subject || !text) {
+      if(!name || !subject || !text || !emailContact) {
         throw new Error("Debes cumplimentar todos los campos")
       }
       
-      await sendMailService({token, name, text, subject }); 
+      await sendMailService({token, name, emailContact, text, subject }); 
       setName("");
+      setEmailContact("");
       setText("");
       setSubject("");
       setShowModal(true);
@@ -44,15 +46,19 @@ export const Form = ()=> {
     <h2 className="contact__title">Ayúdanos a mejorar</h2>
     <form className="contact__form" onSubmit={handleSubmit}>
       {error ? <p className="error">{error}</p> : null}
-      <label htmlFor="name" className="contact__label contact__label--name">Nombre del usuario:</label>
+      <label htmlFor="name" className="contact__label contact__label--name">Nombre del usuario: <span>*</span></label>
       <input value={name} type="text" id="name" className="contact__input contact__input--name" placeholder="Escribe tu nombre..." onChange={(e) => {
         setError("");
         setName(e.target.value)}}/>
-      <label htmlFor="subject" className="contact__label contact__label--subject">Asunto:</label>
+      <label htmlFor="emailContact" className="contact__label contact__label--email">Correo de contacto: <span>*</span></label>
+      <input value={emailContact} type="email" id="emailContact" className="contact__input contact__input--email" placeholder="Escribe tu correo..." onChange={(e) => {
+        setError("");
+        setEmailContact(e.target.value)}}/>
+      <label htmlFor="subject" className="contact__label contact__label--subject">Asunto: <span>*</span></label>
       <input value={subject} type="text" id="subject" className="contact__input contact__input--subject" placeholder="Escribe el asunto del correo..." onChange={(e) => {
         setError("");
         setSubject(e.target.value)}}/>
-      <label htmlFor="textarea" className="contact__label contact__label--textarea" hidden></label>
+      <label htmlFor="textarea" className="contact__label contact__label--textarea">Mensaje <span>*</span></label>
       <textarea value={text} className="contact__textarea" name="textarea" id="textarea" cols="40" rows="15" placeholder="Escribe aquí la información que crees que nos falta..." onChange={(e)=> {
         setError("");
         setText(e.target.value)}}></textarea>
