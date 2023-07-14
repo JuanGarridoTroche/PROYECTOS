d0be9901ad1103dedd08ca65b9f52b11fda47209
Juan Garrido Troche <j.garridotroche@gmail.com>
2023-07-14 22:17:33 +0200

    portfolio - backend - feat: Creando el backend de nuestro nuevo proyecto

--- 12-portfolio/backend/server.js
+++ 12-portfolio/backend/server.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors')
+require('dotenv').config();
 
+const { PORT, HOST } = process.env;
 
-
-
+// Creamos el servidor express a través de su método
 const app = express();
+
+// Middleware para mostrar cualquier petición al servidor en nuestro terminal
+app.use(morgan('dev'));
+
+// Middleware para realizar la conexión entre nuestro back y front
+app.use(cors());
 
 
 //Levantamos el servidor en el puerto
-app.listen(4000, ()=> {
-  console.log("Server listening at http://localhost:4000");
-})
+app.listen(PORT, () => {
+  console.log(`Server listening at ${HOST}:${PORT}`);
+});
