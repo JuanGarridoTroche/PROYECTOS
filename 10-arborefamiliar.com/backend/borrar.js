a662dade7bbca3670465126b7ef85ee02dc9fa9e
Juan Garrido Troche <j.garridotroche@gmail.com>
2023-07-24 23:27:51 +0200

    arborefamiliar.com backend -feat: actualización de los servicios

--- 10-arborefamiliar.com/backend/borrar.js
+++
@@ -1,5 +0,0 @@
-// const data = require("./assets/lineages.json")
-const pass = "irjfgndiof8";
-const array = [{id: 1, password: "irjfgndiof1"}, {id: 2, password: "irjfgndiof2"},{id: 3, password: "irjfgndiof3"}, {id: 4, password: "irjfgndiof4"},{id: 5, password: "irjfgndiof5"},{id: 6, password: "irjfgndiof6"},{id: 7, password: "irjfgndiof7"},{id: 8, password: "irjfgndiof8"},{id: 9, password: "irjfgndiof9"}];
-const result = array.find(e => e.password === pass);
-// console.log(result);
